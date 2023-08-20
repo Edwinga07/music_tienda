4ca9902252d5926479d24ad9d38afb4ff8a0892b
Edwinga07 <edwingalindez0607@gmail.com>
2023-08-19 20:46:21 -0500

    19/08/23

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,10 @@ document.addEventListener('DOMContentLoaded', function(){
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, options);
-  });
+});
+
+var options = {
+    indicator: true,
+    numVisible: 5,
+    padding: 15
+}
