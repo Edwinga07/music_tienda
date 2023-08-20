1ea29760828bc34c72aac2d3363018d62447c226
Edwinga07 <edwingalindez0607@gmail.com>
2023-08-19 20:23:31 -0500

    19/08/23

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// navbar init
+document.addEventListener('DOMContentLoaded', function(){
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+    console.log(instances);
+});
+
+//carousel init
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
